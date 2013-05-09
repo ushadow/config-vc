9b645316f67c8522c926f25ee3b4976142ff52a6
Ying Yin <clevershadow@gmail.com>
2013-05-09 15:54:49 -0400

    Added linking to pathogen.vim to the install script.

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 VIMRC=.vimrc
 ln -sf $PWD/vim/$VIMRC ~/$VIMRC
 echo "Made soft link of $VIMRC"
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+ln -sf $PWD/vim/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+echo "Made soft link of ./vim/pathogen.vim"
