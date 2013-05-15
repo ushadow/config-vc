9b7f5565e10611abac327bb3dad93f6b26cc8275
Ying Yin <clevershadow@gmail.com>
2013-05-15 10:15:37 -0400

    Added checking of OS type in installation script.

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ VIMRC=.vimrc
 ln -sf $PWD/vim/$VIMRC ~/$VIMRC
 echo "Made soft link of $VIMRC"
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-ln -sf $PWD/vim/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
-echo "Made soft link of ./vim/pathogen.vim"
+VIM_DIR=.vim
+
+if [[ $OSTYPE == 'msys' ]]; then
+  VIM_DIR=vimfiles
+fi
+
+mkdir -p ~/$VIM_DIR/autoload ~/$VIM_DIR/bundle
+
+PATHOGEN=$PWD/vim/pathogen/autoload/pathogen.vim 
+PATHOGEN_LINK=~/$VIM_DIR/autoload/pathogen.vim
+
+ln -sf $PATHOGEN $PATHOGEN_LINK
+echo "Made sym link $PATHOGEN_LINK -> $PATHOGEN"
