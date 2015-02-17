54b68fe9df83386c7a6425eb535375c5015aa02d
Ying Yin <clevershadow@gmail.com>
2015-02-16 21:08:35 -0500

    Updated comments.

--- awesome/custom_widgets/init.lua
+++ awesome/custom_widgets/init.lua
@@ -2,15 +2,13 @@
 -- Vicious widgets for the awesome window manager
 ---------------------------------------------------
 -- Licensed under the GNU General Public License v2
---  * (c) 2010, Adrian C. <anrxc@sysphere.org>
 ---------------------------------------------------
 
 -- {{{ Setup environment
 local setmetatable = setmetatable
 local wrequire = require("vicious.helpers").wrequire
 
--- Vicious: widgets for the awesome window manager
--- vicious.contrib
+-- Custom vicious widgets for the awesome window manager
 local custom_widgets = { _NAME = "custom_widgets" }
 -- }}}
 
